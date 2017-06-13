581977f4b7c39ffef59659cfd577e6a67f381cc1
0845855 <0845855@hr.nl>
2017-06-13 20:16:34 +0200

    Create block.ts

--- dev/misc/block.ts
+++ dev/misc/block.ts
@@ -23,22 +23,3 @@ namespace Block{
         }
     }
 }
-
-/*class Block extends MiscItems {
-
-    public x:number;
-    public y:number;
-    public width: number;
-    public height: number;
-            
-    constructor(parent: HTMLElement) {
-        super("block", parent);
-
-    // VERBETERING: Block is nu een child van MiscItems. Hierdoor wordt de speed en de draw en het aanmaken daar afgehandeld. 
-        this.x = 800;
-        this.y = 240;
-
-        this.width = 32;
-        this.height = 31;
-    }
-}*/
